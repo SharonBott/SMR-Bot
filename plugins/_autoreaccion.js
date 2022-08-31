8719744c2952499f67174e08e2e8a3b703549102
Jair Gonzales <108778872+jairgonzales3@users.noreply.github.com>
2022-08-31 15:15:51 -0500

    Update _autoreaccion.js

--- plugins/_autoreaccion.js
+++ plugins/_autoreaccion.js
@@ -5,7 +5,7 @@ react: {
 text: emot,
 key: m.key
 }})}
-handler.customPrefix = /(jhuz|notjhuz - MD|notjhuz - bot|notjhuz-bot)/i
+handler.customPrefix = /(@ffxryu.18|Ryu|Ryu?|Ryu bot)/i
 handler.command = new RegExp
 export default handler
 function pickRandom(list) {
